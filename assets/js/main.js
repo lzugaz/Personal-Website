6e243e57ec9f5567c510f1e9f24583812bd1e2af
lugaz <103002336+lugaz@users.noreply.github.com>
2024-10-17 21:35:59 -0400

    46

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,20 @@ if(navClose){
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+    : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
 
